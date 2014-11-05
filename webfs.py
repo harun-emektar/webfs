f7528f6577f7debb23df26b9bd6e526cdca00895
harun-emektar <harun-emektar@users.noreply.github.com>
2014-11-05 21:34:30 +0100

    Update webfs.py

--- webfs.py
+++ webfs.py
@@ -12,4 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import fusepy
+# http://fuse.sourceforge.net/wiki/index.php/FusePython
+import fuse
+
+class Webfs(fuse.Fuse):
+  pass
